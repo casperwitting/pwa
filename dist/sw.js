ec70c886b31fe087f7207d4509dae4aa5d6cab2e
C.L. Witting <casper.witting@hva.nl>
2017-11-19 15:59:50 +0100

    Added shoppingcart cookie

---
+++ dist/sw.js
@@ -0,0 +1,80 @@
+importScripts('workbox-sw.prod.v2.1.0.js');
+
+/**
+ * DO NOT EDIT THE FILE MANIFEST ENTRY
+ *
+ * The method precache() does the following:
+ * 1. Cache URLs in the manifest to a local cache.
+ * 2. When a network request is made for any of these URLs the response
+ *    will ALWAYS comes from the cache, NEVER the network.
+ * 3. When the service worker changes ONLY assets with a revision change are
+ *    updated, old cache entries are left as is.
+ *
+ * By changing the file manifest manually, your users may end up not receiving
+ * new versions of files because the revision hasn't changed.
+ *
+ * Please use workbox-build or some other tool / approach to generate the file
+ * manifest which accounts for changes to local files and update the revision
+ * accordingly.
+ */
+const fileManifest = [
+  {
+    "url": "3rdpartylicenses.txt",
+    "revision": "f6f06759f30feec078796b20d0a76c21"
+  },
+  {
+    "url": "assets/loader.svg",
+    "revision": "e3076756d6f82b724f917ebf7a689f88"
+  },
+  {
+    "url": "favicon.ico",
+    "revision": "b9aa7c338693424aae99599bec875b5f"
+  },
+  {
+    "url": "fontawesome-webfont.674f50d287a8c48dc19b.eot",
+    "revision": "674f50d287a8c48dc19ba404d20fe713"
+  },
+  {
+    "url": "fontawesome-webfont.912ec66d7572ff821749.svg",
+    "revision": "912ec66d7572ff821749319396470bde"
+  },
+  {
+    "url": "fontawesome-webfont.af7ae505a9eed503f8b8.woff2",
+    "revision": "af7ae505a9eed503f8b8e6982036873e"
+  },
+  {
+    "url": "fontawesome-webfont.b06871f281fee6b241d6.ttf",
+    "revision": "b06871f281fee6b241d60582ae9369b9"
+  },
+  {
+    "url": "fontawesome-webfont.fee66e712a8a08eef580.woff",
+    "revision": "fee66e712a8a08eef5805a46892932ad"
+  },
+  {
+    "url": "index.html",
+    "revision": "ed9ccda38ad896ecbc340ba62b50d3c1"
+  },
+  {
+    "url": "inline.7f6a78687f6eef6efc9c.bundle.js",
+    "revision": "ed5b8e6199b6f75ed8bc5d0d08a4aa84"
+  },
+  {
+    "url": "main.b648c005a0a0205c3191.bundle.js",
+    "revision": "f8b19c2d262f88028e12c9fe42f19cfa"
+  },
+  {
+    "url": "polyfills.900aaf0d520c2ec873a7.bundle.js",
+    "revision": "a1a42d9eb5149e7037b56e5b0ac3d08e"
+  },
+  {
+    "url": "styles.ccac3dbff61cd19109ee.bundle.css",
+    "revision": "ccac3dbff61cd19109eecd8d566e595b"
+  },
+  {
+    "url": "vendor.80221842b737d21944cd.bundle.js",
+    "revision": "4928b1116767b90c089bba0982b14c41"
+  }
+];
+
+const workboxSW = new self.WorkboxSW();
+workboxSW.precache(fileManifest);
