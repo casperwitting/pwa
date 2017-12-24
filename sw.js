da430c7058bfdf6d3767fee8c958f9d0e6456289
Casper Witting <casper@create.nl>
2017-12-24 17:47:49 +0100

    Added firebase to service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
 importScripts('workbox-sw.prod.v2.1.0.js');
-
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+firebase.initializeApp({
+    'messagingSenderId': 'YOUR-SENDER-ID'
+});
+const messaging = firebase.messaging();
 /**
  * DO NOT EDIT THE FILE MANIFEST ENTRY
  *
